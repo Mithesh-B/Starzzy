d6b9b57b7f0c61e12471bf73585cf8037884d4d8
Mithesh B <mitheshbangera70@gmail.com>
2023-02-22 11:38:02 +0530

    add product section

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -4,7 +4,22 @@ import "./products.scss"
 const Products = () => {
   return (
     <div className='products'>
-      Products
+      <div className="left">
+        <div className="filterItem">
+          <h2>Product Categories</h2>
+          <div className="inputItem">
+            <input type="checkbox" id='1' value={1} />
+            <label htmlFor="1">Shoes</label>
+          </div>
+        </div>
+        <div className="filterItem">
+          <h2>Filter by price</h2>
+        </div>
+        <div className="filterItem">
+          <h2>Sort by</h2>
+        </div>
+      </div>
+      <div className="right"></div>
     </div>
   )
 }
