9fe2679d0ddee0b33059751d913c30b08e5d19cd
Mithesh B <mitheshbangera70@gmail.com>
2023-02-22 11:37:29 +0530

    add category section

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Categories from '../../components/Categories/Categories'
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from "../../components/Slider/Slider"
 import "./home.scss"
@@ -8,6 +9,7 @@ const Home = () => {
     <div className='home'>
       <Slider/>
       <FeaturedProducts type='featured'/>
+      <Categories/>
       <FeaturedProducts type='trending'/>
     </div>
   )
