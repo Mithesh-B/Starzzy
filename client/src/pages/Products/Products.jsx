4f71fe43c7139f02bb904209caf51543ed0339ac
Mithesh B <mitheshbangera70@gmail.com>
2023-02-23 15:27:50 +0530

    implement products section

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
+import List from '../../components/List/List'
 import "./products.scss"
 
 const Products = () => {
+
+  const catId = parseInt(useParams().id)
+  const [maxPrice, setMaxPrice] = React.useState(1000);
+  const [sort, setSort]= React.useState(null)
+
   return (
     <div className='products'>
       <div className="left">
@@ -11,15 +18,43 @@ const Products = () => {
             <input type="checkbox" id='1' value={1} />
             <label htmlFor="1">Shoes</label>
           </div>
+          <div className="inputItem">
+            <input type="checkbox" id='2' value={2} />
+            <label htmlFor="2">Skirts</label>
+          </div>
+          <div className="inputItem">
+            <input type="checkbox" id='3' value={3} />
+            <label htmlFor="3">Coats</label>
+          </div>
         </div>
         <div className="filterItem">
           <h2>Filter by price</h2>
+          <div className="inputItem">
+            <span>0</span>
+              <input type="range" min={0} max ={1000} onChange={(e) => setMaxPrice(e.target.value)} />
+            <span>{maxPrice}</span>
+          </div>
         </div>
         <div className="filterItem">
           <h2>Sort by</h2>
+          <div className="inputItem">
+            <input type="radio" id='asc' value="asc" name='price' onChange={e=>setSort("asc")} />
+            <label htmlFor="asc">Price (lowest first)</label>
+          </div>
+          <div className="inputItem">
+            <input type="radio" id='desc' value="desc" name='price' onChange={e=>setSort("desc")} />
+            <label htmlFor="desc">Price (highest first)</label>
+          </div>
         </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+      <img
+          className="catImg"
+          src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
+          alt=""
+        />
+        <List catId={catId} maxPrice={maxPrice} sort={sort}/>
+      </div>
     </div>
   )
 }
