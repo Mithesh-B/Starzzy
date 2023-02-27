de60cc7e0e9a5533672c99a7192f615b24141c99
Mithesh B <mitheshbangera70@gmail.com>
2023-02-27 12:35:09 +0530

    fix onClick event

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -5,12 +5,16 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import BalanceIcon from '@mui/icons-material/Balance';
 import useFetch from '../../hooks/useFetch';
 import { useParams } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../../redux/cartReducer';
 
 const Product = () => {
 
   const id = useParams().id
   const [selectedImg, setSelectedImg]= React.useState("img")
   const [quantity, setQuantity]= React.useState(1)
+
+  const dispatch = useDispatch()
 
   const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
@@ -38,7 +42,11 @@ const Product = () => {
           {quantity}
           <button onClick={()=>setQuantity(prev=>prev+1)}>+</button>
         </div>
-        <button className='add'>
+        <button className='add' onClick={()=>dispatch(addToCart({id:data.id,
+        title: data.attributes.title,
+        price: data.attributes.price,
+        img:data.attributes.img.data.attributes.url,
+        quantity}))}>
           <AddShoppingCartIcon/> ADD TO CART
         </button>
         <div className="links">
